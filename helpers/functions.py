5564fbad674763c7af0ae953a62620f825045b52
Mschaid <68658862+Mschaid@users.noreply.github.com>
2022-10-21 08:22:32 -0500

    initial stage and set up

--- helpers/functions.py
+++ helpers/functions.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+
+
 def process_missing(df):
     """Handle various missing values from the data set
 
@@ -6,9 +10,10 @@ def process_missing(df):
 
     holdout = process_missing(holdout)
     """
-    df["Fare"] = df["Fare"].fillna(train["Fare"].mean())
+    df["Fare"] = df["Fare"].fillna(df["Fare"].mean())
     df["Embarked"] = df["Embarked"].fillna("S")
     return df
+
 
 def process_age(df):
     """Process the Age column into pre-defined 'bins' 
@@ -19,10 +24,12 @@ def process_age(df):
     train = process_age(train)
     """
     df["Age"] = df["Age"].fillna(-0.5)
-    cut_points = [-1,0,5,12,18,35,60,100]
-    label_names = ["Missing","Infant","Child","Teenager","Young Adult","Adult","Senior"]
-    df["Age_categories"] = pd.cut(df["Age"],cut_points,labels=label_names)
+    cut_points = [-1, 0, 5, 12, 18, 35, 60, 100]
+    label_names = ["Missing", "Infant", "Child",
+                   "Teenager", "Young Adult", "Adult", "Senior"]
+    df["Age_categories"] = pd.cut(df["Age"], cut_points, labels=label_names)
     return df
+
 
 def process_fare(df):
     """Process the Fare column into pre-defined 'bins' 
@@ -32,10 +39,11 @@ def process_fare(df):
 
     train = process_fare(train)
     """
-    cut_points = [-1,12,50,100,1000]
-    label_names = ["0-12","12-50","50-100","100+"]
-    df["Fare_categories"] = pd.cut(df["Fare"],cut_points,labels=label_names)
+    cut_points = [-1, 12, 50, 100, 1000]
+    label_names = ["0-12", "12-50", "50-100", "100+"]
+    df["Fare_categories"] = pd.cut(df["Fare"], cut_points, labels=label_names)
     return df
+
 
 def process_cabin(df):
     """Process the Cabin column into pre-defined 'bins' 
@@ -47,8 +55,9 @@ def process_cabin(df):
     """
     df["Cabin_type"] = df["Cabin"].str[0]
     df["Cabin_type"] = df["Cabin_type"].fillna("Unknown")
-    df = df.drop('Cabin',axis=1)
+    df = df.drop('Cabin', axis=1)
     return df
+
 
 def process_titles(df):
     """Extract and categorize the title from the name column 
@@ -59,13 +68,13 @@ def process_titles(df):
     train = process_titles(train)
     """
     titles = {
-        "Mr" :         "Mr",
+        "Mr":         "Mr",
         "Mme":         "Mrs",
         "Ms":          "Mrs",
-        "Mrs" :        "Mrs",
-        "Master" :     "Master",
+        "Mrs":        "Mrs",
+        "Master":     "Master",
         "Mlle":        "Miss",
-        "Miss" :       "Miss",
+        "Miss":       "Miss",
         "Capt":        "Officer",
         "Col":         "Officer",
         "Major":       "Officer",
@@ -73,16 +82,17 @@ def process_titles(df):
         "Rev":         "Officer",
         "Jonkheer":    "Royalty",
         "Don":         "Royalty",
-        "Sir" :        "Royalty",
+        "Sir":        "Royalty",
         "Countess":    "Royalty",
         "Dona":        "Royalty",
-        "Lady" :       "Royalty"
+        "Lady":       "Royalty"
     }
-    extracted_titles = df["Name"].str.extract(' ([A-Za-z]+)\.',expand=False)
+    extracted_titles = df["Name"].str.extract(' ([A-Za-z]+)\.', expand=False)
     df["Title"] = extracted_titles.map(titles)
     return df
 
-def create_dummies(df,column_name):
+
+def create_dummies(df, column_name):
     """Create Dummy Columns (One Hot Encoding) from a single Column
 
     Usage
@@ -90,6 +100,6 @@ def create_dummies(df,column_name):
 
     train = create_dummies(train,"Age")
     """
-    dummies = pd.get_dummies(df[column_name],prefix=column_name)
-    df = pd.concat([df,dummies],axis=1)
+    dummies = pd.get_dummies(df[column_name], prefix=column_name)
+    df = pd.concat([df, dummies], axis=1)
     return df
